880ea918fabae2c7197699bb61a47b7788bc08d0
VSilva <vitor.silva@globalquakemodel.org>
2017-11-10 16:19:45 +0100

    Update calculation scripts

---
+++ groundShakingCalculation.py
@@ -0,0 +1,200 @@
+import math
+import numpy as np
+import time
+from scipy.stats import truncnorm
+from scipy import interpolate
+
+
+def calculate_distance_matrix(exposureLocations):
+
+	noLocations = len(exposureLocations)
+	distanceMatrix = np.zeros((noLocations,noLocations))
+	
+	for i in range(noLocations):
+		for j in range(noLocations):
+			distanceMatrix[i][j]=calculate_distance_2points(exposureLocations[i],exposureLocations[j])
+	
+	return distanceMatrix
+	
+def calculate_single_spatial_correlation_matrix(distanceMatrix,IMT,correlationType):
+	
+	noLocations = len(distanceMatrix)
+	correlationMatrix = np.zeros((noLocations,noLocations))
+	b = calculate_spatial_length_scale(IMT,'Vs30clustered')
+	
+	for i in range(noLocations):
+		for j in range(noLocations):
+			if i == j:
+				correlationMatrix[i][j] = 1
+			else:
+				if correlationType == 'no correlation':
+					correlationMatrix[i][j] = 0
+				if correlationType == 'full correlation':
+					correlationMatrix[i][j] = 0.99999
+				if correlationType == 'spatial':
+					correlationMatrix[i][j]=math.exp(-3*distanceMatrix[i][j]/b)
+	
+	return correlationMatrix
+	
+def calculate_spatial_correlation_matrices(distanceMatrix,IMTs,correlationType):
+	
+	noLocations = len(distanceMatrix)
+	noIMTs = len(IMTs)
+	spatialCorrMatrices = []
+	
+	for i in range(noIMTs):
+		spatialCorrMatrices.append(calculate_single_spatial_correlation_matrix(distanceMatrix,IMTs[i],correlationType))
+			
+	return np.array(spatialCorrMatrices)
+	
+def calculate_spatial_covariance_matrices(closestGroundShaking,spatialCorrMatrices):
+	
+    noIMT = len(spatialCorrMatrices)
+    noLocations = len(spatialCorrMatrices[0])
+    spatialCovMatrices = []
+    for i in range(noIMT):
+        tempCovMatrix = np.zeros((noLocations,noLocations))
+        for j in range(noLocations):
+            for k in range(noLocations):
+                tempCovMatrix[j][k]=spatialCorrMatrices[i][j][k]*closestGroundShaking[j][2+noIMT+i]*closestGroundShaking[k][2+noIMT+i]
+        spatialCovMatrices.append(tempCovMatrix)
+	
+    return np.array(spatialCovMatrices)
+		
+def calculate_cross_correlation_matrix(listIMT,correlationType):
+	
+	noIMT = len(listIMT)
+	crossCorrMatrix = np.zeros((noIMT,noIMT))
+	
+	for i in range(noIMT):
+		if listIMT[i] == 'PGA':
+			T1 = 0.05
+		elif listIMT[i][0:2] == 'SA':
+			T1 = float(listIMT[i].replace("SA(","").replace(")",""))
+		
+		for j in range(noIMT):
+			if listIMT[j] == 'PGA':
+				T2 = 0.05
+			elif listIMT[j][0:2] == 'SA':
+				T2 = float(listIMT[j].replace("SA(","").replace(")",""))
+				
+			if i == j:
+				crossCorrMatrix[i][j] = 1
+			else:
+			
+				Tmax = max([T1,T2]);
+				Tmin = min([T1,T2]);
+
+				if Tmin<0.189:
+					II = 1
+				else:
+					II = 0
+					
+				if correlationType == 'no correlation':
+					crossCorrMatrix[i][j] = 0
+				if correlationType == 'full correlation':
+					crossCorrMatrix[i][j] = 0.99999
+				if correlationType == 'cross':
+					crossCorrMatrix[i][j] = 1-math.cos((math.pi/2)-(0.359+0.163*II*math.log(Tmin/0.189))*math.log(Tmax/Tmin))
+			
+	return crossCorrMatrix
+	
+def generate_random_fields_ground_motion(IMTs,groundShaking,spatialCovMatrices,crossCorrMatrix,siteEffects,noSigmas,noGMFs):
+	
+    noLocations = spatialCovMatrices.shape[1]
+    noIMT = crossCorrMatrix.shape[0]
+    L = []
+    LLT = []
+    Z = []
+	
+    for i in range(noIMT):
+       L.append(np.linalg.cholesky(spatialCovMatrices[i]))
+ 
+    L=np.array(L)
+    
+    for i in range(noIMT):
+        LLTrow = []
+        for j in range(noIMT):
+            LLTrow.append(np.dot(L[i],np.transpose(L[j]))*crossCorrMatrix[i][j])
+        for irow in range(len(LLTrow[0])):
+            singleLLTrow = np.zeros((int(len(LLTrow)*len(LLTrow[0]))))
+            for iL in range(len(LLTrow)):
+                singleLLTrow[iL*len(LLTrow[0]):(iL+1)*len(LLTrow[0])] = LLTrow[iL][irow]
+            LLT.append(singleLLTrow)
+    LLT=np.array(LLT)
+    
+    mu = []
+    for i in range(noIMT):
+        for j in range(noLocations):
+            mu.append(np.ones(noGMFs)*groundShaking[j][i+2])
+    mu = np.array(mu)
+    L = (np.linalg.cholesky(LLT))
+    Z = truncnorm.rvs(-noSigmas, noSigmas, loc=0, scale=1, size=(noLocations*noIMT,noGMFs))
+        
+    gmfs = np.exp(np.dot(L,Z)+mu)
+
+    if siteEffects:
+        gmfs = amplifyGMFs(IMTs,groundShaking[:,-1],gmfs)*0.8
+
+    return gmfs
+    
+def amplifyGMFs(IMTs,Vs30s,gmfs):
+    
+    noLocations = len(Vs30s)
+    
+    for i in range(4):
+        if IMTs[i] == 'PGA':
+            T = 0.0
+        elif IMTs[0:2] == 'SA':
+            IMT = IMTs[i]
+            T = float(IMT.replace("SA(","").replace(")",""))
+            
+        for iloc in range(noLocations):
+            gmfs[i*noLocations+iloc] = amplify_ground_shaking(T,Vs30s[iloc],gmfs[i*noLocations+iloc]) 
+    
+    return gmfs
+	
+def calculate_spatial_length_scale(IMT,Vs30Case):
+	
+	if IMT == 'PGA':
+		T = 0.0
+	elif IMT[0:2] == 'SA':
+		T = float(IMT.replace("SA(","").replace(")",""))
+		
+	if T < 1:
+		if Vs30Case != 'Vs30clustered':
+			b = 8.5+17.2*T
+		elif Vs30Case == 'Vs30clustered':
+			b = 40.7-15.0*T
+	elif T >= 1:
+		b = 22.0 + 3.7*T
+	
+	return b
+	
+def calculate_distance_2points(point1,point2):
+	
+	lon1 = point1[0]
+	lon2 = point2[0]
+	lat1 = point1[1]
+	lat2 = point2[1]
+	
+	lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+	dlon = lon2 - lon1 
+	dlat = lat2 - lat1 
+	a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+
+	distance = 6367*2*math.asin(math.sqrt(a)) 
+	return distance
+    
+def amplify_ground_shaking(T,Vs30,IMLs):
+
+    ampFactorsShort = [(760/Vs30)**0.35,(760/Vs30)**0.35,(760/Vs30)**0.25,(760/Vs30)**0.10,(760/Vs30)**-0.05,(760/Vs30)**-0.05]
+    ampFactorsMid = [(760/Vs30)**0.65,(760/Vs30)**0.65,(760/Vs30)**0.60,(760/Vs30)**0.53,(760/Vs30)**0.45,(760/Vs30)**0.45]
+
+    if T<=0.3:
+		interpolator = interpolate.interp1d([-1,0.1,0.2,0.3,0.4,100],ampFactorsShort, kind = 'linear')
+    if T>0.3:
+		interpolator = interpolate.interp1d([-1,0.1,0.2,0.3,0.4,100],ampFactorsMid, kind = 'linear')
+    
+    return interpolator(IMLs)*IMLs
+        
---
+++ inputProcessing.py
@@ -0,0 +1,316 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import urllib2  # the lib that handles the url stuff
+import math
+import numpy
+import scipy
+import time
+import zipfile
+import urllib
+import os
+
+def save_online_data_to_xml(eventID):
+	
+	try:    
+		groundShakingURL = 'http://shakemap.rm.ingv.it/shake/'+str(eventID)+'/download/grid.xml'
+		groundShakingData = urllib2.urlopen(groundShakingURL) 
+		write_xml(groundShakingData,'groundShaking'+str(eventID))
+	except:
+		groundShakingURL = 'http://shakemap.rm.ingv.it/shake/'+str(eventID)+'/download/grid.xml.zip'
+		groundShakingData = urllib2.urlopen(groundShakingURL) 
+		output = open('groundShaking'+str(eventID)+'.zip', "w")
+		output.write(groundShakingData.read())
+		output.close() 
+        
+	try:
+		uncertaintyURL = 'http://shakemap.rm.ingv.it/shake/'+str(eventID)+'/download/uncertainty.xml'
+		uncertaintyData = urllib2.urlopen(uncertaintyURL)         
+		testfile = urllib.URLopener()
+		testfile.retrieve(uncertaintyURL, "temp.zip")
+		zip_ref = zipfile.ZipFile("temp.zip", 'r')
+		zip_ref.extractall('.')
+		zip_ref.close()
+		os.rename('uncertainty.xml','uncertainty'+str(eventID)+'.xml')
+        
+	except:
+		uncertaintyURL = 'http://shakemap.rm.ingv.it/shake/'+str(eventID)+'/download/uncertainty.xml.zip'
+		uncertaintyData = urllib2.urlopen(uncertaintyURL)         
+		testfile = urllib.URLopener()
+		testfile.retrieve(uncertaintyURL, "temp.zip")
+		zip_ref = zipfile.ZipFile("temp.zip", 'r')
+		zip_ref.extractall('.')
+		zip_ref.close()
+		os.rename('uncertainty.xml','uncertainty'+str(eventID)+'.xml')
+
+
+def write_xml(data,filename):
+
+	out_file = open(filename+'.xml',"w")
+	for line in data:
+	    out_file.write(line)
+	out_file.close()        
+    
+
+def cropShakingData(groundShakingData,typeCrop,limit):
+
+    groundShakingData = numpy.array(groundShakingData)
+    
+    lon = groundShakingData[:,0]
+    lat = groundShakingData[:,1]
+    
+    if typeCrop == 'radius':
+        epicentre = [scipy.mean(lon),scipy.mean(lat)]
+    
+        for iloc in range(len(lon)-1,0,-1):
+            distance_x = math.fabs(epicentre[0]-lon[iloc])*111*math.cos(lat[iloc]*math.pi/180)
+            distance_y = math.fabs(epicentre[1]-lat[iloc])*111
+            distance = math.sqrt(distance_x**2 + distance_y**2)
+            if distance > limit:
+                groundShakingData = numpy.delete(groundShakingData, iloc, 0)
+                
+    elif typeCrop == 'box':
+        for iloc in range(len(lon)-1,0,-1):
+            if limit[0] > lon[iloc] or limit[1] < lon[iloc] or limit[2] > lat[iloc] or limit[3] < lat[iloc] :
+                groundShakingData = numpy.delete(groundShakingData, iloc, 0)
+                
+    
+    print 'Total number of locations after cropping'
+    print len(groundShakingData)
+    
+    return groundShakingData
+    
+def reduceShakingData(groundShakingData,box,res):
+
+    groundShakingData = numpy.array(groundShakingData)
+    lon = numpy.linspace(box[0],box[1],(box[1]-box[0])/res)
+    print lon
+    lat = numpy.linspace(box[2],box[3],(box[3]-box[2])/res)
+    reducedGroundShaking = numpy.zeros((int(len(lat)*len(lon)),len(groundShakingData[0])))
+
+    counter = 0
+    for ilon in range(len(lon)):
+        for ilat in range(len(lat)):
+            reducedGroundShaking[counter][0] = lon[ilon]
+            reducedGroundShaking[counter][1] = lat[ilat]
+            reducedGroundShaking[counter][2:] = findClosestData(lon[ilon],lat[ilat],groundShakingData)
+            counter = counter + 1
+    
+    print 'Total number of locations after re-distribution'
+    print len(reducedGroundShaking)
+    
+    return reducedGroundShaking
+    
+def findClosestData(lon,lat,groundShakingData):
+
+    dist = numpy.array(((groundShakingData[:,0]-lon)**2+(groundShakingData[:,1]-lat)**2)**0.5)
+    idx = numpy.where(dist == dist.min())
+    
+    return groundShakingData[idx[0],2:]
+    
+def exportLocation(croppedShakingData):
+    
+    data = numpy.array(croppedShakingData)
+    locations = data[:,0:2]
+    
+    return locations
+	
+def parse_xml_data_ID(eventID):
+	
+    groundShakingFile='groundShaking'+str(eventID)+'.xml'
+    uncertaintyFile='uncertainty'+str(eventID)+'.xml'
+    data, IMTs = parse_xml_data_files(groundShakingFile,uncertaintyFile)
+    
+    return data, IMTs
+    
+def parse_xml_data_files(groundShakingFile,uncertaintyFile):
+
+    data = []
+    file=open(groundShakingFile)
+    lines1=file.readlines()
+    file=open(uncertaintyFile)
+    lines2=file.readlines()
+    
+    for i in range(len(lines1)):
+        line1 = lines1[i].split()
+        if isfloat(line1[0]):
+            firstFloat1 = i
+            break
+    
+    for i in range(len(lines2)):
+        line2 = lines2[i].split()
+        if isfloat(line2[0]):
+            firstFloat2 = i
+            break
+
+    if len(lines1[firstFloat1].split()) > 9:
+        IMTs = ['PGA','SA(0.3)','SA(1.0)','SA(3.0)']
+    else:
+        IMTs = ['PGA']
+    
+    for i in range(len(lines1)):
+        line1 = lines1[i].split()
+        if isfloat(line1[0]):
+            line2 = lines2[i+firstFloat2-firstFloat1].split()
+            lon = float(line1[0])
+            lat = float(line1[1])
+            if float(line1[2]) > 0:
+                sPGA = float(line2[2])
+                #sPGA = 0.62
+                m = float(line1[2])/100
+                mPGA = math.log(m**2/math.sqrt((m**2*(math.exp(sPGA**2)-1))+m**2))
+                if len(line1) > 9:
+                    sSa03 = float(line2[5])
+                    #sSa03 = 0.69
+                    m = float(line1[5])/100
+                    mSa03 = math.log(m**2/math.sqrt((m**2*(math.exp(sSa03**2)-1))+m**2))
+                    
+                    sSa10 = float(line2[6])
+                    #sSa10 = 0.7
+                    m = float(line1[6])/100
+                    mSa10 = math.log(m**2/math.sqrt((m**2*(math.exp(sSa10**2)-1))+m**2))
+                    
+                    sSa30 = float(line2[7])
+                    #sSa30 = 0.69
+                    m = float(line1[7])/100
+                    mSa30 = math.log(m**2/math.sqrt((m**2*(math.exp(sSa30**2)-1))+m**2))
+                      
+                    Vs30 = float(line1[10])
+                    data.append([lon,lat,mPGA,mSa03,mSa10,mSa30,sPGA,sSa03,sSa10,sSa30,Vs30])
+                else:
+                    Vs30 = float(line1[7])
+                    data.append([lon,lat,mPGA,sPGA,Vs30])
+    file.close
+    
+    print 'Total number of locations with ground shaking:'
+    print len(data)
+    print 'The following IMTs were found:'
+    print IMTs
+	
+    return data, IMTs
+	
+def calculateMuSigma(m,sd):
+	
+	mu = math.log(m**2/math.sqrt(sd**2 + m**2))
+	sigma = math.sqrt(math.log(sd**2/m**2+1))
+	
+	return mu, sigma
+
+def extract_location_exposure(exposureModel):
+
+    assetLocations = []
+    uniqueLocations = []
+
+    file=open(exposureModel)
+    lines=file.readlines()
+    for line in lines:
+        if 'location' in line:
+            lon = extract_attribute(line,'lon')
+            lat = extract_attribute(line,'lat')
+            assetLocations.append([float(lon),float(lat)])
+    file.close
+
+    for i in range(len(assetLocations)):
+        if assetLocations[i] not in uniqueLocations:
+            uniqueLocations.append(assetLocations[i])
+            
+    uniqueLocations = numpy.array(uniqueLocations)
+			
+    return uniqueLocations[uniqueLocations[:,0].argsort()]
+
+#    return uniqueLocations
+
+def extract_IMTs_fragility(fragilityModel):
+
+	IMTs = []
+	uniqueIMTs = []
+	file=open(fragilityModel)
+	lines=file.readlines()
+	for line in lines:
+		if 'IMT' in line:
+			IMT = extract_attribute(line,'IMT')
+			IMTs.append(IMT)
+	file.close
+	
+	for i in range(len(IMTs)):
+		if IMTs[i] not in uniqueIMTs:
+			uniqueIMTs.append(IMTs[i])
+			
+	return uniqueIMTs
+	
+def extract_closest_ground_shaking(exposureLocations,groundShaking):
+
+	groundShaking = numpy.array(groundShaking)
+	exposureLocations = numpy.array(exposureLocations)
+	closestGroundShaking = numpy.zeros((exposureLocations.shape[0],groundShaking.shape[1]))
+
+	for i in range(exposureLocations.shape[0]):
+		location = numpy.array(exposureLocations[i])
+		dist_2 = numpy.sum((groundShaking[:,0:2] - location)**2, axis=1)
+		closestGroundShaking[i]=groundShaking[numpy.argmin(dist_2)]
+	return closestGroundShaking
+	
+def extract_attribute(line,attribute):
+	
+	position = line.find(attribute)
+	subLine = line[position+len(attribute)+2:-1]
+	
+	return subLine[0:subLine.index('"')]
+	
+def isfloat(value):
+	
+	try:
+		float(value)
+		return True
+	except ValueError:
+		return False
+        
+def plotLocations(locations):
+    
+    locations = numpy.array(locations)
+    box = define_bounding_box(locations[:,0:2])
+    plt.figure(3, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+    map = Basemap(llcrnrlon=box["lon_1"],llcrnrlat=box["lat_1"],
+        urcrnrlon=box["lon_2"],urcrnrlat=box["lat_2"],projection='mill',resolution='i')
+    x, y = map(locations[:,0],locations[:,1])
+    
+    map.drawcoastlines(linewidth=0.25)
+    map.drawcountries(linewidth=0.25)
+    map.drawstates(linewidth=0.25)
+    map.drawmapboundary(fill_color='aqua')
+    map.fillcontinents(color='white',lake_color='aqua')
+    plt.scatter(x,y,5,marker='o',color='k',zorder=4)
+
+    if box["lat_length"]+2 < 4:
+        parallels = numpy.arange(0.,81,0.5)
+    else:
+        parallels = numpy.arange(0.,81,1.0)
+    map.drawparallels(parallels,labels=[True,False,True,False])
+    if box["lon_length"]+2 < 4:
+        meridians = numpy.arange(0.,360,0.5)
+    else:
+        meridians = numpy.arange(0.,360,1.0)
+    map.drawmeridians(meridians,labels=[True,False,False,True])
+
+    plt.show()
+    
+def define_bounding_box(locations):
+
+    box = {"lon_0": None, "lat_0": None, "lon_1": None, 
+        "lat_1": None, "lon_2": None, "lat_2": None}
+
+    maxCoordinates = locations.max(axis=0)
+    minCoordinates = locations.min(axis=0)
+    lengthLon = abs(maxCoordinates[0]-minCoordinates[0])
+    lengthLat = abs(maxCoordinates[1]-minCoordinates[1])
+
+    box["lon_0"] = (minCoordinates[0]+maxCoordinates[0])/2
+    box["lat_0"] = (minCoordinates[1]+maxCoordinates[1])/2
+    box["lon_1"] = minCoordinates[0]-1
+    box["lat_1"] = minCoordinates[1]-1
+    box["lon_2"] = maxCoordinates[0]+1
+    box["lat_2"] = maxCoordinates[1]+1
+    box["lon_length"] = lengthLon
+    box["lat_length"] = lengthLat
+
+    return box
